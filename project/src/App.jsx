beefe7fa67b4f5ddbb848d386d3d88126a567ad6
Ajaykumar026 <your-email@gmail.com>
2026-05-02 23:14:33 +0530

    install react and tailwind css and make component

---
+++ project/src/App.jsx
@@ -0,0 +1,18 @@
+import Home from "./component/Home";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Card from "./component/Card";
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
